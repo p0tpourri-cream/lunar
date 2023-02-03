7dd3310b6f072d8c6899ae46787403d2eb4f8e3f
p0tpourri-cream <1428085661@qq.com>
2023-02-03 22:20:03 +0800

    makefile

---
+++ build/CMakeFiles/sylar.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../lib/libsylar.pdb"
+  "../lib/libsylar.so"
+  "CMakeFiles/sylar.dir/sylar/logger.cc.o"
+  "CMakeFiles/sylar.dir/sylar/logger.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sylar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
